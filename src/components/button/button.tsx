5ad4bf269d029fa02908a3c600033b36c1d17c6c
karimim <muhammad.karimi@post.ch>
2026-03-13 16:43:49 +0100

    feat: add Story and Types for Button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,18 +1,49 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, AnchorHTMLAttributes, ElementType } from "react";
 import styles from "./button.module.css";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonBaseProps {
   variant?: "primary" | "secondary";
   size?: "md" | "lg";
+  as?: ElementType;
 }
+
+interface ButtonAsButton
+  extends ButtonBaseProps, ButtonHTMLAttributes<HTMLButtonElement> {
+  as?: "button";
+}
+
+interface ButtonAsAnchor
+  extends ButtonBaseProps, AnchorHTMLAttributes<HTMLAnchorElement> {
+  as?: "a";
+  href: string;
+}
+
+type ButtonProps = ButtonAsButton | ButtonAsAnchor;
 const Button = ({
   variant = "primary",
   size = "md",
   className,
+  as: Component = "button",
   ...props
 }: ButtonProps) => {
-  const combinedClasses = [styles[variant], styles[size], className].join(" ");
-  return <Button className={combinedClasses} {...props} />;
+  const combinedClasses = [styles[variant], styles[size], className]
+    .filter(Boolean)
+    .join(" ");
+
+  if (Component === "a") {
+    return (
+      <Component
+        className={combinedClasses}
+        {...(props as AnchorHTMLAttributes<HTMLAnchorElement>)}
+      />
+    );
+  }
+  return (
+    <Component
+      className={combinedClasses}
+      {...(props as ButtonHTMLAttributes<HTMLButtonElement>)}
+    />
+  );
 };
 
 export { Button };
