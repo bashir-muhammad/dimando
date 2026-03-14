9baaff8c4429e51e446299be3f6addabe467e193
karimim <muhammad.karimi@post.ch>
2026-03-14 23:04:44 +0100

    feat: add AppContext and cards to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
+"use client";
 import styles from "@/styles/page.module.css";
+import { useApp } from "@/context/app-context";
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/card/card";
+import DoubleArrowRight from "@/assets/icons/double-arrow-right.svg";
 
 export default function Home() {
+  const { state } = useApp();
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h2>Heading</h2>Main page
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At hic
-          nostrum cum facere molestiae, voluptas nobis perferendis voluptate
-          temporibus ex culpa dolorum doloribus odio nemo eligendi ipsa facilis
-          error rem!
-        </p>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1
+        className={styles.title}
+        dangerouslySetInnerHTML={{ __html: state.config?.homepage.title ?? "" }}
+      />
+      <p>{state.config?.homepage.description}</p>
+
+      <div className={styles.cards}>
+        {state.config?.questionnaires.map((quesionnare) => (
+          <Card
+            key={quesionnare.id}
+            variant="default"
+            style={{ backgroundColor: quesionnare.color }}
+          >
+            <CardTitle>{quesionnare.title}</CardTitle>
+            <CardDescription>{quesionnare.description}</CardDescription>
+            <CardFooter icon={<DoubleArrowRight width={20} height={20} />}>
+              <span>
+                {quesionnare.questions.length} Question
+                {quesionnare.questions.length > 1 && "s"}
+              </span>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
