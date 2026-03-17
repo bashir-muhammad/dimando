147f6a596f6d569b944b34cec51207db1f295744
karimim <muhammad.karimi@post.ch>
2026-03-17 15:31:36 +0100

    feat: add footer data and styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,13 +2,31 @@ import { HTMLAttributes } from "react";
 
 import Styles from "./footer.module.css";
 
-const Footer = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+interface FooterProps extends HTMLAttributes<HTMLDivElement> {
+  data?: {
+    copyright?: string;
+    links?: {
+      url: string;
+      name: string;
+    }[];
+  };
+}
+
+const Footer = ({ className, data, ...props }: FooterProps) => {
   const combinedClasses = [Styles.footer, className].filter(Boolean).join(" ");
   return (
-    <div className={combinedClasses} {...props}>
-      <span className={Styles.yellowbar}>Q - All rights reserved 2026</span>
-      <div className={Styles.logo}>Privacy Policy | Terms of Use</div>
-    </div>
+    <footer className={combinedClasses} {...props}>
+      <span className={Styles.yellowbar}>
+        {data?.copyright ?? "Q - All rights reserved 2026."}
+      </span>
+      <div className={Styles.links}>
+        {data?.links?.map((link) => (
+          <a key={link.url} href={link.url}>
+            {link.name}
+          </a>
+        ))}
+      </div>
+    </footer>
   );
 };
 
